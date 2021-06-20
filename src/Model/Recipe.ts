80f37b908a34cd3e52b0a56d3f89c0d7ea7f687f
Inbal Schkolnik <capoeira0012>
2021-06-20 11:19:59 +0300

    Aגג strength + prettier

--- src/Model/Recipe.ts
+++ src/Model/Recipe.ts
@@ -1,72 +1,84 @@
+/* eslint-disable prettier/prettier */
 import AbstractInventoryItem from './InventoryModel/AbstractInventoryItem';
 import { NullInventoryItem } from './InventoryModel/InventoryItem';
 
 class Recipe {
-    name: string;
-    method: [string];
-    portion: number;
-    //categories: Array<string>;
-    // TODO measurement converting ( + measure unit for misc InItems)
-    // TODO garnish
-    // TODO parse ingredients
+  name: string;
+  method: [string];
+  portion: number;
+  strength: number;
+  //categories: Array<string>;
+  // TODO measurement converting ( + measure unit for misc InItems)
+  // TODO garnish
+  // TODO parse ingredients
 
-    constructor(name: string, public ingredients: [number, AbstractInventoryItem, boolean][], method: [string], portion: number) {
-        this.name = name;
-        this.method = method;
-        this.portion = portion;
-        //this.categories = new Array(ERecipeCategory.StrengthNonAlcoholic);
-    }
+  constructor(
+    name: string,
+    public ingredients: [number, AbstractInventoryItem, boolean][],
+    method: [string],
+    portion: number,
+  ) {
+    this.name = name;
+    this.method = method;
+    this.portion = portion;
+    //this.categories = new Array(ERecipeCategory.StrengthNonAlcoholic);
+  }
 
-    addCategory(category: string) {
-        // TODO
-    }
+  addCategory(category: string) {
+    // TODO
+  }
 
-    toJson() {
-        return JSON.stringify({
-            name: this.name,
-            ingredients: this.ingredientsToJson(),
-            method: this.method,
-            portion: this.portion,
-            //categories: JSON.stringify(this.categories),
-        })
-    }
+  toJson() {
+    return JSON.stringify({
+      name: this.name,
+      ingredients: this.ingredientsToJson(),
+      method: this.method,
+      portion: this.portion,
+      //categories: JSON.stringify(this.categories),
+    });
+  }
 
-    ingredientsToJson() {
-        var str: string = "[";
+  ingredientsToJson() {
+    let str = '[';
 
-        this.ingredients.forEach(item => {
-            str += item[0] + " " + item[1].name + ", ";
-        });
-        str += "]"
-        //str+=this.ingredients.map(ing => {return (<li>{ing[0]} {ing[1].getName()}</li>)})
-        return str;
-    }
+    this.ingredients.forEach((item) => {
+      str += item[0] + ' ' + item[1].name + ', ';
+    });
+    str += ']';
+    //str+=this.ingredients.map(ing => {return (<li>{ing[0]} {ing[1].getName()}</li>)})
+    return str;
+  }
 }
 
 class NullRecipe extends Recipe {
-    constructor() {
-        super("Recipe Not Found", [[0, new NullInventoryItem(), false]], ["Recipe Not Found"], 0);
-    }
+  constructor() {
+    super(
+      'Recipe Not Found',
+      [[0, new NullInventoryItem(), false]],
+      ['Recipe Not Found'],
+      0,
+    );
+  }
 
-    addCategory(category: string) {
-        // TODO
-    }
+  addCategory(category: string) {
+    // TODO
+  }
 
-    checkAvailability() {
-        return { success: false, reason: "Recipe Not Found" };
-    }
+  checkAvailability() {
+    return { success: false, reason: 'Recipe Not Found' };
+  }
 
-    calculateDrinkStrength() {
-        return 0;
-    }
+  calculateDrinkStrength() {
+    return 0;
+  }
 
-    toJson() {
-        return JSON.stringify("Recipe Not Found");
-    }
+  toJson() {
+    return JSON.stringify('Recipe Not Found');
+  }
 
-    ingredientsToJson(): string {
-        return "Recipe Not Found";
-    }
+  ingredientsToJson(): string {
+    return 'Recipe Not Found';
+  }
 }
 export default Recipe;
 export { NullRecipe };
